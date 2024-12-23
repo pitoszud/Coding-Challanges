9b9c5c9d0556457dd0b12f2034ec6691b4dd4fb3
Patryk Jakubik <pjakubik@ybs.co.uk>
2024-12-23 15:03:15 +0000

    Flow exception handling

--- coding-challanges-v2/src/main/kotlin/coroutines_revisited/FlowOp.kt
+++ coding-challanges-v2/src/main/kotlin/coroutines_revisited/FlowOp.kt
@@ -1,11 +1,13 @@
 package coroutines_revisited
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import kotlin.coroutines.EmptyCoroutineContext
 
-fun main() {
+suspend fun main() = coroutineScope {
 
     val intFlow = flow {
         delay(100)
@@ -13,20 +15,70 @@ fun main() {
         delay(200)
         emit(2)
         delay(300)
+        throw RuntimeException("Flow failed")
         emit(3)
+        delay(400)
+        emit(4)
     }
 
     val scope = CoroutineScope(EmptyCoroutineContext)
 
     intFlow
-        .map { it.toDouble() }
-        .onStart { println("Starting flow") }
-        .onEach { it * 1.123 }
+        .map { it.toDouble() * 1.123 }
+        .onStart { println("---- Starting flow 1 ----") }
         .onCompletion { cause ->
-            
+            if (cause != null) {
+                println("Flow completed exceptionally")
+            } else {
+                println("Flow completed successfully")
+            }
         }
-        .launchIn(scope)
+        .onEach {
+            println("Received: $it")
+        }
+        .catch { t ->
+            println("Caught exception: ${t.message}")
+            emit(0.0)
+            emitAll(fallbackFlow().map { it.toDouble() })
+        }
+        .launchIn(scope) // shorthand for scope.launch { flow. collect() }
 
+
+
+    intFlow
+        .map { it.toDouble() * 1.123 }
+        .onStart { println("----- Starting flow 2 -----") }
+        .onCompletion { cause ->
+            if (cause != null) {
+                println("Flow completed exceptionally")
+            } else {
+                println("Flow completed successfully")
+            }
+        }
+        .onEach {
+            println("Received: $it")
+        }
+        .catch { t ->
+            println("Caught exception: ${t.message}")
+            emit(0.0)
+            emitAll(fallbackFlow().map { it.toDouble() })
+        }
+        .collect {
+            println("Received from collect: $it")
+        }
+
+    Thread.sleep(2000)
+
+}
+
+fun fallbackFlow(): Flow<Int> {
+    return flow {
+        emit(5)
+        delay(100)
+        emit(6)
+        delay(200)
+        emit(7)
+    }
 }
 
 sealed class Output {
