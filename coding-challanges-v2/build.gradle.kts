22423ddaa868f50ba836fe5c23d8f70ba76ad921
Patryk Jakubik <pjakubik@ybs.co.uk>
2025-11-06 16:41:47 +0000

    Add datetime examples and dependency for kotlinx-datetime

--- coding-challanges-v2/build.gradle.kts
+++ coding-challanges-v2/build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.7.1")
     testImplementation("junit:junit:4.13.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
     //testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
---
+++ coding-challanges-v2/src/main/kotlin/date_time/DateTimeExamples.kt
@@ -0,0 +1,19 @@
+package date_time
+
+import kotlinx.datetime.LocalDateTime
+import kotlin.time.Duration
+
+fun main() {
+
+}
+
+
+fun timeBetweenTwoDateTimesJava() {
+    val startDateTime = LocalDateTime(2023, 1, 1, 12, 20)
+    val endDateTime = LocalDateTime(2024, 6, 15, 15, 45)
+
+}
+
+fun timeBetweenTwoDatesKotlin() {
+
+}
