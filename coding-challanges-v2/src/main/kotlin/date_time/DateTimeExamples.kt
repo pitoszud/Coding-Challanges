dc512b95efaf2db7b794203da99edeb68cca796d
Patryk Jakubik <pitos007@gmail.com>
2025-11-08 21:33:07 +0000

    Update Kotlin version and enhance datetime examples with new functions

--- coding-challanges-v2/src/main/kotlin/date_time/DateTimeExamples.kt
+++ coding-challanges-v2/src/main/kotlin/date_time/DateTimeExamples.kt
@@ -1,19 +1,54 @@
 package date_time
 
-import kotlinx.datetime.LocalDateTime
-import kotlin.time.Duration
+import kotlinx.datetime.*
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+import kotlin.time.Instant
 
 fun main() {
-
+    timeBetweenTwoDatesKotlinB()
 }
 
 
-fun timeBetweenTwoDateTimesJava() {
-    val startDateTime = LocalDateTime(2023, 1, 1, 12, 20)
-    val endDateTime = LocalDateTime(2024, 6, 15, 15, 45)
-
+@OptIn(ExperimentalTime::class)
+fun longToLocalDateTime() {
+    val startLong = 1762634228000L
+    val startInstant = Instant.fromEpochMilliseconds(startLong)
+    val startLocalDateTime: LocalDateTime = startInstant.toLocalDateTime(TimeZone.currentSystemDefault())
 }
 
-fun timeBetweenTwoDatesKotlin() {
 
+@OptIn(ExperimentalTime::class)
+fun timeBetweenTwoDatesKotlinB() {
+    val startSeconds = 1762634228L
+    val startInstant = Instant.fromEpochSeconds(startSeconds)
+
+    val now = Clock.System.now()
+    val dateTimePeriod = startInstant.periodUntil(now, TimeZone.currentSystemDefault())
+    val monthsDiff = startInstant.until(now, DateTimeUnit.MONTH, TimeZone.currentSystemDefault())
+
+    val days = dateTimePeriod.days
+    val months = dateTimePeriod.months
+    val years = dateTimePeriod.years
+    val hours = dateTimePeriod.hours
+    val minutes = dateTimePeriod.minutes
+
+    println("Difference is $years years, $months months, $days days, $hours hours, $minutes minutes")
+}
+
+/*
+* The recommended way to use a LocalDateTime is to treat it as a representation of an Instant,
+* perform all the required arithmetic on Instant values,
+* and only convert to LocalDateTime when a human-readable representation is needed.
+* */
+@OptIn(ExperimentalTime::class)
+fun addPeriodToInstant() {
+    // Arithmetic on Instant
+    val now = Clock.System.now()
+    val systemTZ = TimeZone.currentSystemDefault()
+    val nextMonth = now.plus(2, DateTimeUnit.MONTH, systemTZ)
+    val nextYearAndMonth = now.plus(DateTimePeriod(years = 1, months = 1), systemTZ)
+
+    // to LocalDateTime
+    val nextMonthLocalDateTime = nextMonth.toLocalDateTime(systemTZ)
 }
